931ee526a31833facdb49c4508eb5c13cf668b40
a-barret <aidenb360@gmail.com>
2025-05-20 13:24:07 -0600

    Initial Commit

---
+++ myWeatherIOS/myWeatherIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  myWeatherIOSApp.swift
+//  myWeatherIOS
+//
+//  Created by Aiden Barrett on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct myWeatherIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myWeatherIOSTests/myWeatherIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  myWeatherIOSTests.swift
+//  myWeatherIOSTests
+//
+//  Created by Aiden Barrett on 5/20/25.
+//
+
+import Testing
+@testable import myWeatherIOS
+
+struct myWeatherIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
